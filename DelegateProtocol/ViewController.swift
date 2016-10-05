e851dc4f669fb6f65a486f3f29c4217affba957d
Alvin Joseph Valdez <alvinvaldez@macintoshs-macbook-pro-4.local>
2016-10-06 02:27:53 +0800

    Initial Commit

---
+++ DelegateProtocol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DelegateProtocol
+//
+//  Created by Alvin Joseph Valdez on 06/10/2016.
+//  Copyright © 2016 Alvin Joseph Valdez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
